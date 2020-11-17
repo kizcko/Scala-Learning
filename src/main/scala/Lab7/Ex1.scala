16c0661ef1b3721a1b350a4e8bab9ac669750f53
Mija Andrei <62014570+kizcko@users.noreply.github.com>
2020-11-17 09:50:42 +0200

    Add files via upload

--- src/main/scala/Lab7/Ex1.scala
+++ src/main/scala/Lab7/Ex1.scala
@@ -1,15 +1,45 @@
 package Lab7
 
 import com.cra.figaro.language._
-import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.library.atomic.{discrete,continuous}
+import com.cra.figaro.library.collection.FixedSizeArray
+import com.cra.figaro.algorithm.sampling.Importance
 
-object Ex1 {
+
+object golf{
 	def main(args: Array[String]) {
-		val test = Constant("Test")
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
+		val golfHoles = new FixedSizeArray(18, i =>discrete.Uniform(3, 4, 5));
+		val skill = continuous.Uniform(0.0, 8.0 / 13.0)
+
+		val score = golfHoles.chain((par: Int) => 
+        Chain(skill, (s:Double) => Select(
+              s / 8 -> (par - 2),
+              s / 2 -> (par - 1),
+              s -> par,
+              4.0 / 5.0 * (1.0 - 13.0 * s / 8.0) -> (par+1),
+              1.0 / 5.0 * (1.0 - 13.0 * s / 8.0) -> (par+2)  
+            )
+          )
+       ) .foldLeft(0)(_+_)
+
+
+	var alg = Importance(10000, score)
+    alg.start()
+    alg.stop()
+	val probMaiMareCa80 = alg.probability(score, (s:Int) => s >= 80)
+	alg.kill()
+
+	skill.setCondition((i: Double) => i >= 0.3)
+	alg.start()
+    alg.stop()
+	val probSkillMaiMareCa03 = alg.probability(score, (s:Int) => s >= 80)
+	alg.kill()
+
+	println("Probabilitatea pentru scor >=80 : " + probMaiMareCa80)
+	println("Probabilitatea pentru skill >= 0.3 : " + probSkillMaiMareCa03)
+
+
 	}
 }
+
