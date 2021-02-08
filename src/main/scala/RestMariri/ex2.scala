cb5cf8b186ee28ff17b2c8f2eed498569b96560d
Mija Andrei <62014570+kizcko@users.noreply.github.com>
2021-02-08 15:12:12 +0200

    Add files via upload

---
+++ src/main/scala/RestMariri/ex2.scala
@@ -0,0 +1,48 @@
+import com.cra.figaro.language.{Select, Element}
+import com.cra.figaro.library.compound.{CPD, RichCPD, OneOf}
+import com.cra.figaro.algorithm.factored.{VariableElimination, MPEVariableElimination}
+import com.cra.figaro.library.atomic.discrete.Uniform
+
+object Test2{
+    //a
+    abstract class State{ 
+        val ascuns: Element[String] 
+        def stare = CPD(ascuns, //in functie de starea ascunsa primeste variabila observata
+                            "A" -> Constant("buna"), 
+                            "B" -> Constant("nu prea buna"),
+                            "C" -> Constant("bolnav"),
+                            "D" -> Constant("decedat"))
+    }
+    //b
+    class InitialState(n: String) extends State{ 
+        val ascuns = Uniform(n)
+    }
+    //c
+    class NextState(curent: State) extends State{
+        val ascuns = CPD(curent, ascuns, //pentru fiecare stare ascunsa selecteaza o stare noua cu probabilitatile indicate
+                         "A" -> Select(0.721 -> "A", 0.202 ->"B", 0.067->"C", 0.1 ->"D"),
+                         "B" -> Select(0.581 ->"B", 0.407->"C", 0.012 ->"D"),
+                         "C" -> Select(0.75->"C", 0.25 ->"D"),
+                         "D" -> Select(1 ->"D"))
+    }
+    //d
+
+    class stateSequence(n: Int): List(State) {   //o functie ce construieste o secventa de stari ale sistemului(fiecare stare reprezinta o stare ascunsa)
+        if (n == 0)
+            List(new InitialState("A")) //Primeste ca parametru prima stare A
+        else{
+            val last :: rest = stateSequence (n-1) //completeaza recursiv restul starilor
+            new NextState(last) :: last :: rest
+        }
+    }
+
+
+    def main(args: Array[String]){
+        val states = 10 //numarul de stari ale sistemului
+        val stateSeq = stateSequence(states) // se creaza secventa de stari
+
+        println(VariableElimination.probability(stateSeq(5).consum, "scazut"))
+
+    }
+
+}
